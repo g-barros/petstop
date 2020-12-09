30139a7ee82330b0f3956f22fdcdab1a635fe4ae
Gustavo Barros <gbarrosfigueiredo@gmail.com>
2020-12-09 07:47:46 -0300

    fix main script

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
 const navbar = document.querySelector('.navbar');
 
- window.addEventListener('scroll', function () {
-    console.log('scrolou')
+window.addEventListener('scroll', function () {
     if ( window.scrollY > 90 ) {
         navbar.classList.add('active');
     } else {
         navbar.classList.remove('active');
     }
 })
-//  on('scroll', function () {
-//         console.log('scrolou')
-//         if ( $(window).scrollTop() > 90 ) {
-//             $('.navbar').addClass('active');
-//         } else {
-//             $('.navbar').removeClass('active');
-//         }
-//     });
